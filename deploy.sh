ba4d14497398eb62b2d1620b2ab13d74247a9207
Fritz <39598547+fritzkeyzer@users.noreply.github.com>
2021-11-25 20:13:41 +0200

    Added comments

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,21 @@
 #go build
 #go test
 
+# GOOGLE CLOUD REPO
+#git add .
+#git commit -a -m"Smiles"
+#git push
 
+# GOOGLE CLOUD FUNCTION (requires gcloud sdk to be installed)
 #gcloud init
 #gcloud functions deploy luno_candle_poll --entry-point LunoCandlePoll --trigger-topic LunoCandlePoll --region europe-west1 --runtime go116 --env-vars-file ./.env.yaml
 #gcloud functions deploy luno_candles_poll --env-vars-file ./.env.yaml
 #gcloud functions delete luno_candles_poll
 
-
+# GOOGLE CLOUD PUB/SUB
 #gcloud pubsub topics create LunoCandlesPoll
 #gcloud pubsub topics publish LunoCandlesPoll --message LunoCandlesPoll
 #gcloud pubsub topics delete LunoCandlesPoll
 
 
-# TODO create cloud scheduler jobs
+# TODO GOOGLE CLOUD SCHEDULER
